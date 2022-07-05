246d1fbded7aea74632fb881f7a472d40ac892b0
joel <78315282+joel710@users.noreply.github.com>
2022-07-05 22:42:29 +0200

    Update upload_document.py

--- bot/core/new/upload_document.py
+++ bot/core/new/upload_document.py
@@ -56,7 +56,7 @@ class UploadDocument:
 
         if not caption:
             caption = f"**File Name:** `{os.path.basename(document)}`" \
-                      "\n\n**@OpusTechz**"
+                      "\n\n**@jojo_dev**"
         c_time = time.time()
         await self.send_document(
             chat_id=chat_id,
