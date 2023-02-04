8b463a86c52de119d98ca555982c027a508b6bc2
joel <78315282+joel710@users.noreply.github.com>
2023-02-04 19:20:09 +0100

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,5 @@
 import bot
 
 if __name__ == "__main__":
-    if hasattr(bot, 'run'):
-        bot.run()
-    else:
-        print("Le module 'bot' ne contient pas de méthode 'run'.")
+    bot.run()
+
